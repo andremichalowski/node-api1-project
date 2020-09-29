005573d440cd94d6abc03454c6f8bd6b0589c923
amichalo421 <andre.michalowski@gmail.com>
2020-09-28 23:19:34 -0400

    POST REQUEST (Test)

--- README.md
+++ README.md
@@ -88,6 +88,7 @@ When the client makes a `POST` request to `/api/users`:
   - respond with HTTP status code `500` (Server Error).
   - return the following JSON object: `{ errorMessage: "There was an error while saving the user to the database" }`.
 
+***
 When the client makes a `GET` request to `/api/users`:
 
 - If there's an error in retrieving the _users_ from the database:
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // implement your API here
 
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
+const { insert, findById, find } = require("./data/db");
+
 //BASIC GET REQUEST
 server.get("/", (req, res) => {
-  res.status(200).json({ hello: "Node 34"})
-})
+  res.status(200).json({ hello: "Node 34" });
+});
 
 //GET REQEUST (LESSONS)
 const lessons = [
@@ -19,18 +21,34 @@ server.get("/lessons", (req, res) => {
   res.status(200).json({ data: lessons });
 });
 
+//POST REQUEST ()
+server.post("/api/users", (req, res) => {
+  const body = req.body;
 
+  if (body.name || body.bio) {
+    const newUser = {
+      id: 1, // TODO generate ID
+      name: body.name,
+      bio: body.bio,
+    };
+
+    insert(newUser)
+      .then(() => res.status(201).json(newUser))
+      .catch(() =>
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the user to the database",
+        })
+      );
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide name and bio for the user.",
+    });
+  }
+});
 
 const port = 3000;
 server.listen(port, () => console.log("server running..."));
-
-
-
-
-
-
-
-
 
 // server.post("/api/users", (req, res) => {
 //   res.status(200).json({ hello: "Users"})
